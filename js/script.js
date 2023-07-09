2d290f12e12aef4256c42f14a72b9abc7c8da3a4
TaccLuc <tacc.luca@gmail.com>
2023-07-09 17:38:23 +0200

    Fix: all the bugs related to notification

--- js/script.js
+++ js/script.js
@@ -9,8 +9,6 @@ createApp({
             newResponse: '',
             receivingUser: '',
             contactWriting: false,
-            unreadMessage: false,
-            newMessages: '',
             hour: NaN,
             minutes: NaN,
             contacts: [
@@ -22,17 +20,20 @@ createApp({
                 {
                 date: '10/01/2020 15:30:55',
                 message: 'Hai portato a spasso il cane?',
-                status: 'sent'
+                status: 'sent',
+                read: true
                 },
                 {
                 date: '10/01/2020 15:50:00',
                 message: 'Ricordati di stendere i panni',
-                status: 'sent'
+                status: 'sent',
+                read: true
                 },
                 {
                 date: '10/01/2020 16:15:22',
                 message: 'Tutto fatto!',
-                status: 'received'
+                status: 'received',
+                read: true
                 }
                 ],
                 },
@@ -44,17 +45,20 @@ createApp({
                 {
                 date: '20/03/2020 16:30:00',
                 message: 'Ciao come stai?',
-                status: 'sent'
+                status: 'sent',
+                read: true
                 },
                 {
                 date: '20/03/2020 16:30:55',
                 message: 'Bene grazie! Stasera ci vediamo?',
-                status: 'received'
+                status: 'received',
+                read: true
                 },
                 {
                 date: '20/03/2020 16:35:00',
                 message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                status: 'sent'
+                status: 'sent',
+                read: true
                 }
                 ],
                 },
@@ -66,17 +70,20 @@ createApp({
                 {
                 date: '28/03/2020 10:10:40',
                 message: 'La Marianna va in campagna',
-                status: 'received'
+                status: 'received',
+                read: true
                 },
                 {
                 date: '28/03/2020 10:20:10',
                 message: 'Sicuro di non aver sbagliato chat?',
-                status: 'sent'
+                status: 'sent',
+                read: true
                 },
                 {
                 date: '28/03/2020 16:15:22',
                 message: 'Ah scusa!',
-                status: 'received'
+                status: 'received',
+                read: true
                 }
                 ],
                 },
@@ -88,12 +95,14 @@ createApp({
                 {
                 date: '10/01/2020 15:30:55',
                 message: 'Lo sai che ha aperto una nuova pizzeria?',
-                status: 'sent'
+                status: 'sent',
+                read: true
                 },
                 {
                 date: '10/01/2020 15:50:00',
                 message: 'Si, ma preferirei andare al cinema',
-                status: 'received'
+                status: 'received',
+                read: true
                 }
                 ],
                 },
@@ -105,12 +114,14 @@ createApp({
                 {
                 date: '10/01/2020 15:30:55',
                 message: 'Ricordati di chiamare la nonna',
-                status: 'sent'
+                status: 'sent',
+                read: true
                 },
                 {
                 date: '10/01/2020 15:50:00',
                 message: 'Va bene, stasera la sento',
-                status: 'received'
+                status: 'received',
+                read: true
                 }
                 ],
                 },
@@ -122,17 +133,20 @@ createApp({
                 {
                 date: '10/01/2020 15:30:55',
                 message: 'Ciao Claudia, hai novità?',
-                status: 'sent'
+                status: 'sent',
+                read: true
                 },
                 {
                 date: '10/01/2020 15:50:00',
                 message: 'Non ancora',
-                status: 'received'
+                status: 'received',
+                read: true
                 },
                 {
                 date: '10/01/2020 15:51:00',
                 message: 'Nessuna nuova, buona nuova',
-                status: 'sent'
+                status: 'sent',
+                read: true
                 }
                 ],
                 },
@@ -144,12 +158,14 @@ createApp({
                 {
                 date: '10/01/2020 15:30:55',
                 message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                status: 'sent'
+                status: 'sent',
+                read: true
                 },
                 {
                 date: '10/01/2020 15:50:00',
                 message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                status: 'received'
+                status: 'received',
+                read: true
                 }
                 ],
                 },
@@ -161,17 +177,20 @@ createApp({
                 {
                 date: '10/01/2020 15:30:55',
                 message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                status: 'received'
+                status: 'received',
+                read: true
                 },
                 {
                 date: '10/01/2020 15:50:00',
                 message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                status: 'sent'
+                status: 'sent',
+                read: true
                 },
                 {
                 date: '10/01/2020 15:51:00',
                 message: 'OK!!',
-                status: 'received'
+                status: 'received',
+                read: true
                 }
                 ],
                 }
@@ -181,18 +200,16 @@ createApp({
     methods:{
         changeActiveUser(i) {
             this.activeUser = i;
-            if (this.activeUser == this.receivingUser) {
-                this.unreadMessage = false;
-                this.newMessages = '';
-                this.newText = '';
-            }
+            this.newText = '';
+            this.contacts[this.activeUser].messages[this.contacts[this.activeUser].messages.length - 1].read = true;
         },
         sendMessage() {
                 if (this.newText.trim() != '') {
                     this.contacts[this.activeUser].messages.push( {
                         date: this.getCurrentTime(),
                         message: this.newText,
-                        status: 'sent'
+                        status: 'sent',
+                        read: true
                     } );
                     this.newText = '';
                     this.receivingUser = this.activeUser;
@@ -216,7 +233,7 @@ createApp({
                 //     this.newText = '';
                 //     this.contactWriting = false;
                 // })
-                const lastMessage = this.contacts[this.activeUser].messages[this.contacts[this.activeUser].messages.length -1].message.toLowerCase();
+                const lastMessage = this.contacts[this.receivingUser].messages[this.contacts[this.receivingUser].messages.length -1].message.toLowerCase();
                 
                 if(lastMessage.includes("ciao")){
                     this.newResponse = 'Ciao a te';
@@ -251,12 +268,11 @@ createApp({
                 this.contacts[this.receivingUser].messages.push({
                         date: this.getCurrentTime(),
                         message: this.newResponse,
-                        status: 'received'
+                        status: 'received',
+                        read: false
                 })
                 this.newText = '';
-                this.contactWriting = false;
-                this.unreadMessage = true;
-                this.newMessages++     
+                this.contactWriting = false;   
         },
         searchContact() {
             for (let i = 0; i < this.contacts.length; i++) {
